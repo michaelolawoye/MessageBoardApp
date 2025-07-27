2831267f9f117d361379d8ff75afa8223b6cbb43
Michael Olawoye <michael2005olawoye@gmail.com>
2025-07-27 00:04:31 +0000

    move bmp image and text using arrow keys. truetypefonts

--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,40 @@
 class LTexture {
 
 	public:
-		LTexture(SDL_Renderer** renderer): cRenderer{renderer}, width{0}, height{0} {}
+
+		static constexpr float originalSize{-1.f};
+
+		LTexture(SDL_Renderer** renderer): cRenderer{renderer}, cWidth{0}, cHeight{0} {}
 
 		~LTexture() { destroy(); }
 
-		bool loadFromFile(std::string path);
+		bool loadImage(std::string path);
+		bool loadText(std::string text, SDL_Color color, TTF_Font* font);
 
 		void destroy() {
 			SDL_DestroyTexture(cTexture);
 			cTexture = nullptr;
 
-			width = 0;
-			height = 0;
+			cWidth = 0;
+			cHeight = 0;
 		}
 
-		void render(float x, float y);
+		void render(SDL_FRect* srcRegion, SDL_FRect* dstRegion);
 
-		int getWidth() { return width; }
-		int getHeight() { return height; }
+		int getWidth() { return cWidth; }
+		int getHeight() { return cHeight; }
 		bool isLoaded() { return cTexture != nullptr; }
 
 	private:
 		SDL_Texture* cTexture;
 		SDL_Renderer** cRenderer;
 
-		int width;
-		int height;
+		int cWidth;
+		int cHeight;
+
 };
 
-bool LTexture::loadFromFile(std::string path) {
+bool LTexture::loadImage(std::string path) {
 
 	// clean up texture if already exists
 	destroy();
@@ -46,46 +51,111 @@ bool LTexture::loadFromFile(std::string path) {
 		return false;
 	}
 
+	// sets colour specified to be transparent on surface given
+	// if (!SDL_SetSurfaceColorKey(surface, true, SDL_MapSurfaceRGB(surface, 0, 0, 0xFF))) {
+
+	// 	SDL_Log("Couldn't set color key. SDL_Error: %s\n", SDL_GetError());
+	// }
+
 	if (cTexture = SDL_CreateTextureFromSurface(*cRenderer, surface); cTexture == nullptr) {
 		SDL_Log("Couldn't create texture. SDL_Error:%s\n", SDL_GetError());
 		return false;
 	}
 
-	width = surface->w;
-	height = surface->h;
+	cWidth = surface->w;
+	cHeight = surface->h;
 
 	SDL_DestroySurface(surface);
 
 	return true;
 }
 
-void LTexture::render(float x, float y) {
+bool LTexture::loadText(std::string text, SDL_Color color, TTF_Font* font) {
 
-	SDL_FRect dstRect{x, y, static_cast<float>(width), static_cast<float>(height)};
+	destroy();
 
-	SDL_RenderTexture(*cRenderer, cTexture, nullptr, &dstRect);
+	SDL_Surface* textSurface{nullptr};
+
+	if (textSurface = TTF_RenderText_Blended(font, text.c_str(), 0, color); textSurface == nullptr) {
+		SDL_Log("Couldn't create text surface. SDL_Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	if (cTexture = SDL_CreateTextureFromSurface(*cRenderer, textSurface); cTexture == nullptr) {
+		SDL_Log("Couldn't create texture. SDL_Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	cWidth = textSurface->w;
+	cHeight = textSurface->h;
+
+	SDL_DestroySurface(textSurface);
+
+	return true;
 }
+
+// srcRegion determines position and size of region cut out of the texture, dstRegion determines the position and size of the srcRegion placed on the screen 
+void LTexture::render(SDL_FRect* srcRegion, SDL_FRect* dstRegion) {
+
+	if (dstRegion->w <= 0 && !srcRegion) {
+		dstRegion->w = cWidth;
+	} else if (dstRegion->w <= 0) {
+		dstRegion->w = srcRegion->w;
+	} 
+
+	if (dstRegion->h <= 0 && !srcRegion) {
+		dstRegion->h = cHeight;
+	} else if (dstRegion->w <= 0) {
+		dstRegion->h = srcRegion->h;
+	}
+
+	// 2nd argument is the texture being cut out of by srcRegion
+	SDL_RenderTexture(*cRenderer, cTexture, srcRegion, dstRegion);
+}
+
 
 int main(int argc, char* argv[]) {
 
-	SDL_Window* mWindow = nullptr;
-	SDL_Renderer* mRenderer = nullptr; 
-	SDL_Surface* mSurface = nullptr;
-	LTexture texture {&mRenderer};
+	SDL_Window* mWindow{nullptr};
+	SDL_Renderer* mRenderer{nullptr}; 
+	SDL_Surface* mSurface{nullptr};
+
+	LTexture imgTexture{&mRenderer};
+	LTexture textTexture{&mRenderer};
+
+	TTF_Font* mFont{nullptr};
+	SDL_Color blackColor{0, 0, 0, 0xFF};
+
+	std::string fontpath{"./assets/Roboto/static/Roboto-Black.ttf"};
 
 	if (!init(&mWindow, &mRenderer, &mSurface)) {
 		return 1;
 	}
 
-	std::string path {"./assets/simplebmp.bmp"};
-	if (!loadMedia(&texture, path)) {
+	if (mFont = TTF_OpenFont(fontpath.c_str(), fontpath.length()); mFont == nullptr) {
+		SDL_Log("Couldn't open font: %s. SDL_Error: %s\n", fontpath.c_str(), SDL_GetError());
 		return 1;
 	}
 
+	std::string sentence{"Hello World"};
+	if (!textTexture.loadText(sentence, blackColor, mFont)) {
+		return 1;
+	}
+
+	std::string path {"./assets/simplebmp.bmp"};
+	if (!imgTexture.loadImage(path)) {
+		return 1;
+	}
 
 	bool quit = false;
 	SDL_Event e;
 	SDL_zero(e);
+
+	constexpr int SIMPLEBMP_WIDTH{200};
+	constexpr int SIMPLEBMP_HEIGHT{200};
+	constexpr int SPEED{10};
+	int texture_x = 0;
+	int texture_y = 0;
 
 	while (!quit) {
 
@@ -101,19 +171,27 @@ int main(int argc, char* argv[]) {
 					switch (e.key.key) {
 						case SDLK_UP:
 							SDL_Log("Up\n");
-							SDL_SetRenderDrawColor(mRenderer, 0, 0, 0xFF, 0xFF);
+
+							if (texture_y > 0) { texture_y-=SPEED; }
+
 							break;
 						case SDLK_DOWN:
 							SDL_Log("Down\n");
-							SDL_SetRenderDrawColor(mRenderer, 0xFF, 0, 0, 0xFF);
+
+							if (texture_y <= SCREEN_HEIGHT-SIMPLEBMP_HEIGHT-SPEED) { texture_y+=SPEED; }
+
 							break;
 						case SDLK_LEFT:
 							SDL_Log("Left\n");
-							SDL_SetRenderDrawColor(mRenderer, 0, 0xFF, 0, 0xFF);
+
+							if (texture_x > 0) { texture_x-=SPEED; }
+
 							break;
 						case SDLK_RIGHT:
 							SDL_Log("Right\n");
-							SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0, 0xFF);
+
+							if (texture_x <= SCREEN_WIDTH-SIMPLEBMP_WIDTH-SPEED) { texture_x+=SPEED; }
+
 							break;
 						default:
 							break;
@@ -124,14 +202,25 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
+		SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(mRenderer);
 
-		texture.render(0.f, 0.f);
+		SDL_FRect spriteClip{0.f, 0.f, 200.f, 200.f};
+
+		SDL_FRect dstRect1{texture_x, texture_y, 0, 0};
+		SDL_FRect dstRect2{300, 100, 200, 50};
+
+		imgTexture.render(nullptr, &dstRect1);
+		imgTexture.render(&spriteClip, &dstRect2);
+
+		textTexture.render(nullptr, &dstRect1);
 		SDL_RenderPresent(mRenderer);
 	}
 
-	close(&mWindow, &mRenderer, &mSurface);
+	TTF_CloseFont(mFont);
+	mFont = nullptr;
 
+	close(&mWindow, &mRenderer, &mSurface);
 
 	return 0;
 }
@@ -154,18 +243,15 @@ bool init(SDL_Window** window, SDL_Renderer** renderer, SDL_Surface** background
 		return false;
 	}
 
-	return true;
-}
-
-
-bool loadMedia(void* texture, std::string path) {
-
-	if (!static_cast<LTexture*>(texture)->loadFromFile(path)) {
+	if (!TTF_Init()) {
+		SDL_Log("Couldn't initialize TTF! SDL_Error: %s\n", SDL_GetError());
 		return false;
 	}
 
 	return true;
 }
+
+
 
 void close (SDL_Window** window, SDL_Renderer** renderer, SDL_Surface** surface) {
 
@@ -178,5 +264,6 @@ void close (SDL_Window** window, SDL_Renderer** renderer, SDL_Surface** surface)
 	SDL_DestroyWindow(*window);
 	window = nullptr;
 
+	TTF_Quit();
 	SDL_Quit();	
 }
