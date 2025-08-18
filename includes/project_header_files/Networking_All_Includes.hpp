1bb27c0346fcdec7ef3f34306029664bb90f96f6
Michael Olawoye <michael2005olawoye@gmail.com>
2025-08-18 22:06:33 +0000

    didn't add this header to previous commit

--- includes/project_header_files/Networking_All_Includes.hpp
+++ includes/project_header_files/Networking_All_Includes.hpp
@@ -10,4 +10,16 @@
 #define MY_PORT 5050
 #include  <stdlib.h>
 
+
+
+void* get_inaddr(struct sockaddr* sa) {
+
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
 #endif
